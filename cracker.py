345cd4be06edae5eb351dc11b74175b168b1cc4e
yegane-fmd <yegane.fmd@gmail.com>
2025-05-25 09:52:35 +0330

    Update cracker.py

--- cracker.py
+++ cracker.py
@@ -1,7 +1,7 @@
 import csv
 import hashlib
 
-# مرحله 1: خواندن فایل CSV و جدا کردن اسم و هش
+
 names = []
 hashes = []
 
@@ -14,11 +14,11 @@ with open('hashes_only.csv', 'r') as file:
         hashes.append(hash_value)
         print(f"Name: {name}, Hash: {hash_value}")
 
-# مرحله 2: حدس زدن عدد 4 رقمی برای هر هش
+
 cracked_passwords = {}
 
 for i in range(10000):
-    password = f"{i:04d}"  # عدد رو به رشته ۴ رقمی تبدیل می‌کنیم (مثلاً 0001، 0234)
+    password = f"{i:04d}"  
     password_hash = hashlib.sha256(password.encode()).hexdigest()
 
     for idx, target_hash in enumerate(hashes):
@@ -28,7 +28,7 @@ for i in range(10000):
                 "hash": target_hash
             }
 
-# مرحله 3: چاپ دیکشنری نهایی
+
 print("\nCracked Passwords:")
 for name, info in cracked_passwords.items():
     print(f"{name}: {info['password']} -> {info['hash']}")
